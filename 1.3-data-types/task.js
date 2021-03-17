b82e4b1785c19a1964ffa22c84b684bc5a0b7def
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-03-17 11:27:21 +0300

    Доработано задание 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,35 +2,29 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // Про принцип программирования YAGNI знаю. 
-    // Сделал дополнительные проверки и функции исключительно с целью отработки практических навыков!
-    // Буду благодарен любой критике. Чем больше, тем благодарнее =))
-
     // Смотрим, не ввёл ли пользователь процентную ставку через запятую;
     // Если ввёл, то заменяем её на точку; 
     // В любом случае извлекаем число с плавающей точкой из строки (в случае, если пользователь указал символ %, например).
-    if (percent.indexOf(',') !== -1) {
+    if (percent.indexOf(',') !== -1 && typeof percent == 'string') {
         percent = parseFloat(percent.replace(',', '.'));
     } else {
         percent = parseFloat(percent);
     }
     contribution = parseInt(contribution);
     amount = parseInt(amount);
-
     // Проверяем, введены ли все значения корректно:
-    if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || percent <= 0 || amount <= 0 || contribution < 0) {
         return 'Заполните все поля корректными значениями!';
     }
-    if (date == 'Invalid Date') {
-        return 'Введите дату окончания ипотеки!';
+    // Сегодняшняя дата
+    let todayDate = new Date();
+    // Считаем количество выплачиваемых месяцев. 
+    let countMonths = (date.getFullYear() - todayDate.getFullYear()) * 12 - (todayDate.getMonth() + 1) + date.getMonth() + 1;
+    if (date == 'Invalid Date' || countMonths < 12) {
+        return 'Введите корректную дату окончания ипотеки!';
     } else {
         // Если всё введено хорошо, то считаем общую стоимость ипотеки:
-        let todayDate = new Date(); // Сегодняшняя дата
         let creditSum = amount - contribution; // Тело кредита (сумма, которую необходимо вернуть банку)
-        // Считаем количество выплачиваемых месяцев. 
-        // Вычисления могут быть с погрешностью по дням, так как считать вплоть до дня можно, но мне очень долго формулу высчитывать.
-        // Такой задачи не стояло ))
-        let countMonths = (date.getFullYear() - todayDate.getFullYear()) * 12 - (todayDate.getMonth() + 1) + date.getMonth() + 1;
         // Вычисляем процентную ставку за месяц (Зачем делить на 100? Указано в задании..)
         let monthlyPercent = percent / 100 / 12;
         //Вычисляем ежемесячную оплату:
@@ -38,15 +32,14 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
         // Итого, общая сумма ипотеки:
         let totalAmount = +(monthlyPayment * countMonths).toFixed(2);
         console.log(totalAmount);
-        return totalAmount;
+        return totalAmount + ' рублей';
     }
 
 }
 
 function getGreeting(name) {
-    if (name) {
-        return `Привет, мир! Меня зовут ${name}.`;
-    } else {
-        return `Привет, мир! Меня зовут Аноним.`;
+    if (!name) {
+        name = 'Аноним';
     }
+    return `Привет, мир! Меня зовут ${name}.`;
 }
